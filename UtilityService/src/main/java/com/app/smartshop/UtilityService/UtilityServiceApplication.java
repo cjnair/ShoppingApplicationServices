a1650bb64d207bf86d224ccd08b9cf172a75d287
cjnair <jayaraj.chevery@gmail.com>
2020-06-13 00:45:47 +0530

    Initial Commit

---
+++ UtilityService/src/main/java/com/app/smartshop/UtilityService/UtilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.smartshop.UtilityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityServiceApplication.class, args);
+	}
+
+}
---
+++ UtilityService/src/test/java/com/app/smartshop/UtilityService/UtilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.smartshop.UtilityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

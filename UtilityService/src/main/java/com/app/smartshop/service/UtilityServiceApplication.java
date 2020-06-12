916edbc42f03e51533f9cbcd7cc131a07611019f
P7111829 <P7111829@BLR-P7111829.NESSINBLR.COM>
2020-06-13 00:59:15 +0530

    Added Package

--- UtilityService/src/main/java/com/app/smartshop/service/UtilityServiceApplication.java
+++ UtilityService/src/main/java/com/app/smartshop/service/UtilityServiceApplication.java
@@ -1,4 +1,4 @@
-package com.app.smartshop.UtilityService;
+package com.app.smartshop.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
